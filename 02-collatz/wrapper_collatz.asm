79f166a95f607f3e6071656ac8ebf59b66f9f1a2
schmis80 <schmis80@zedat.fu-berlin.de>
2018-09-17 15:12:17 +0200

    rename file

---
+++ 02-collatz/wrapper_collatz.asm
@@ -0,0 +1,80 @@
+;************************************************
+; 
+; Rechnerarchitektur (TI2) - WS16
+; Collatz Wrapper
+;
+; Wrapper for Assignment 2:
+;   gets number n passed as argument string,
+;   converts n to unsigned integer,
+;   calls 'collatz'-function,
+;   prints result or error message
+;
+; Stefan Schmid
+; 2018/08/17
+
+%define RED     27,"[31;1m" 
+%define RESET   27,"[0m"
+
+section .bss
+
+end_ptr:    resq    1
+
+
+section .data
+
+result_msg:         
+    db      "collatz(%u) = %llu",10,0
+err_invalid_msg:    
+    db      RED,"Invalid argument: ",RESET,"%s",10,\
+            "Only digits are allowed!",10,0
+err_not_enough_msg:
+    db      RED,"Not enough Arguments",10,RESET,\
+            "Usage: ./collatz <number>",10,0
+
+section .text
+
+extern strtoul, printf, collatz
+global main
+
+main:
+    mov     r12, rsi
+    cmp     rdi, 2
+    jb      err_not_enough
+    mov     rdi, [r12+8]        ;get passed argument
+    mov     rsi, end_ptr
+    mov     rdx, 10             ;base to convert to
+    mov     rax, strtoul
+    call    rax
+    mov     rdi, end_ptr
+    mov     rdi, [rdi]
+    cmp     byte [rdi], 0       ;test if conversion was successfull
+    jne     err_invalid
+    push    rax
+    mov     rdi, rax
+    call    collatz
+    mov     rdi, result_msg
+    pop     rsi
+    mov     rdx, rax
+    xor     rax, rax
+    mov     rcx, printf
+    call    rcx
+    xor     rdi, rdi            ;program was successful
+    jmp     exit
+    
+err_not_enough:
+    mov     rdi, err_not_enough_msg
+    jmp     print_error
+
+err_invalid:
+    mov     rdi, err_invalid_msg
+    mov     rsi, [r12+8]        ;invalid argument
+
+print_error:
+    xor     rax, rax
+    mov     rcx, printf
+    call    rcx
+    mov     rdi, 1              ;program was not successful
+
+exit:
+    mov     rax, 60
+    syscall
